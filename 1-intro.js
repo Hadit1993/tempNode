219888faca4c6129ce7dc529fe447e8f52778383
hadit1993 <hadit1993@gmail.com>
2021-07-13 20:09:11 +0430

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,5 @@
+const amount = 9;
+
+console.log(amount < 10 ? "small number" : "large number");
+
+console.log("hey it's my first node app!!!");
---
+++ 6-modules.js
@@ -0,0 +1,7 @@
+const {john, peter} = require ('./2-names');
+const _sayHi = require ('./3-utils');
+const data = require ('./4-alternative-flavor');
+require ('./5-mind-grenade');
+_sayHi ('susan');
+_sayHi (john);
+_sayHi (peter);
